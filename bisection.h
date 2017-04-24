349c054db14bb66407e6be732439a2d0232811e8
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-04-25 04:00:49 +0900

    upd var name

--- bisection.h
+++ bisection.h
@@ -4,23 +4,23 @@
 #include <math.h>
 #include "funcdef.h"
 
-double getCenter(double minInRange, double maxInRange) {
-    return (minInRange+maxInRange)/2;
+double getCenterInRange(double min, double max) {
+    return (min+max)/2;
 }
 
 double bisectionMethod(double accuracy) {
-    double minInRange = -1, maxInRange = 2;
+    double min = -1, max = 2;
 
     double x;
     while (1) {
-        x = getCenter(minInRange, maxInRange);
+        x = getCenterInRange(min, max);
         double y = f(x);
 
         if (fabs(y) < accuracy) break;
         if (signbit(y)) {
-            minInRange = x;
+            min = x;
         } else {
-            maxInRange = x;
+            max = x;
         }
     }
 
