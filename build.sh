a7c5ce3ce2cdf120cc3bf10ed4c87e555855b4b4
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-05-06 23:23:24 +0900

    add build & clean.sh

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc main.c funcdef.h bisection.h secant.h secant.c newton-raphson.h newton-raphson.c -lm
---
+++ clean.sh
@@ -0,0 +1,4 @@
+rm a.out bisection.h.gch funcdef.h.gch newton-raphson.h.gch secant.h.gch
+
+
+
