c46628346b930e86602edfe81affa6dbd0793bb2
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-08-04 18:30:54 +0900

    upd main output

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "bisection.h"
 #include "secant.h"
 #include "newton-raphson.h"
 
 int main() {
-    printf("'4x-xe^(2x)+2 = 0' ");
-    printf("solve with Newton-Raphson method.\n");
-
+    puts("'4x-xe^(2x)+2 = 0' solve with Newton-Raphson method.");
     printf("x = %.8lf\n", f_newton_raphson());
 
-    printf("'x-cos(x) = 0' ");
-    printf("solve with Bisection, Secant & Newton-Raphson method.\n");
+    puts("'x-cos(x) = 0' solve with Bisection, Secant & Newton-Raphson method.");
+    printf("Bisection method: x = %.8lf\n", bisection());
+    printf("Secant method: x = %.8lf\n", secant());
+    printf("Newton-Raphson method: x = %.8lf\n", g_newton_raphson());
 
-    printf("x = %.8lf\n", bisection());
-    printf("x = %.8lf\n", secant());
-    printf("x = %.8lf\n", g_newton_raphson());
-
-    return 0;
+    return EXIT_SUCCESS;
 }
