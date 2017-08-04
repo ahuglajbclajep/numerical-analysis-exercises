78998a188ca54ed35903d9c1b0bfc5fefe2b68bf
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-08-04 22:54:15 +0900

    upd use static inline

--- funcdef.h
+++ funcdef.h
@@ -3,19 +3,22 @@
 
 #include <math.h>
 
-static double f(double x) {
+// When it is an argument of a higher-order function,
+// the inline function is output as a normal static function.
+
+static inline double f(double x) {
     return 4*x -x*exp(2*x) +2;
 }
 
-static double df(double x) {
+static inline double df(double x) {
     return 4 -exp(2*x) -2*x*exp(2*x);
 }
 
-static double g(double x) {
+static inline double g(double x) {
     return x -cos(x);
 }
 
-static double dg(double x) {
+static inline double dg(double x) {
     return sin(x) +1;
 }
 
